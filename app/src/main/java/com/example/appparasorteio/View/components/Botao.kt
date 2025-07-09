4bef1668d3a68682db11361341a6570cf4abae89
Denisenogueira1997 <denogueira102@hotmail.com>
2025-07-09 16:30:28 -0300

    Pequenas alterações

--- app/src/main/java/com/example/appparasorteio/View/components/Botao.kt
+++ app/src/main/java/com/example/appparasorteio/View/components/Botao.kt
@@ -8,32 +8,37 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
-import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.unit.dp
-import com.example.appparasorteio.R
-import com.example.appparasorteio.ui.theme.Blue80
+
 
 @Composable
-fun BotaoTelaInicial(onClick: () -> Unit) {
-    Button(onClick = onClick,
+fun Botao(
+    texto:String,
+    icone: ImageVector,
+    onClick: () -> Unit
+){
+    Button(
+        onClick = onClick,
         shape = RoundedCornerShape(16.dp),
         colors = ButtonDefaults.buttonColors(
-            containerColor = Blue80,
-            contentColor = Color.White
+            containerColor = MaterialTheme.colorScheme.primary,
+            contentColor = MaterialTheme.colorScheme.onPrimary,
+
         )
     ){
         Icon(
-            imageVector = ImageVector.vectorResource(id = R.drawable.add),
+            imageVector = icone,
             contentDescription = "",
             modifier = Modifier.size(20.dp),
-            tint = Color.White
+            tint = MaterialTheme.colorScheme.onPrimary
         )
         Spacer(modifier = Modifier.width(8.dp))
-        Text(text = "{button text}", color = Color.White)
+        Text(text = texto, color = MaterialTheme.colorScheme.onPrimary)
     }
 }
